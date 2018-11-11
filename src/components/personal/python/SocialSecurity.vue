62d365c0ebae9aa072720fe865f2c8b553fcf5e0
Spanky(Mac) <819062398@qq.com>
2018-11-11 22:20:36 +0800

    社保1

--- src/components/personal/python/SocialSecurity.vue
+++ src/components/personal/python/SocialSecurity.vue
@@ -14,13 +14,13 @@
         </el-row>
         <el-row>
           <el-col :span="10">
-            <el-form-item label="所属机构" label-width="0" prop="org" class="form-item">
+            <el-form-item label="所属机构（灰色为不可用）" label-width="0" prop="org" class="form-item">
               <el-select v-model="orgArea" placeholder="所属省份" @change="inputFrom.org=undefined">
                 <el-option v-for="(f, index) in socialsecurityList" :key="index" :label="f.name" :value="index"></el-option>
               </el-select>
               <el-select v-model="inputFrom.org" no-data-text="请先选择所属省份">
                 <template v-if="!isNaN(orgArea)">
-                  <el-option v-for="(o, index) in socialsecurityList[orgArea].childOrgs" :key="index" :label="o.name" :value="o.org_id"></el-option>
+                  <el-option v-for="(o, index) in socialsecurityList[orgArea].childOrgs" :key="index" :label="o.name" :value="o.org_id" :disabled="o.status=='DISABLED'"></el-option>
                 </template>
               </el-select>
             </el-form-item>
@@ -30,6 +30,7 @@
           <el-col :span="4">
             <el-form-item>
               <el-button type="primary" @click="onSubmit" round size="small">执行查询</el-button>
+              <el-button type="primary" @click="inputOrgInfo" round size="small">获取机构信息</el-button>
             </el-form-item>
           </el-col>
         </el-row>
@@ -45,7 +46,7 @@
       </div>
       <table class="table card-text">
         <tr class="text-left">
-          <td width="20%">匹配结果</td>
+          <td width="20%">匹配结果{{openId}}</td>
           <td><el-tag type="success" class="tag">成功</el-tag></td>
         </tr>
       </table>
@@ -54,9 +55,11 @@
 </template>
 
 <script>
+import SsLogin from './SsLogin';
 export default {
   name: 'SocialSecurity',
   components: {
+    SsLogin
   },
   data () {
     return {
@@ -65,21 +68,90 @@ export default {
         org: ''
       },
       rules: {
-        partnerUserId: [{ required: true, message: '请输入paetnerID', trigger: 'blur' }]
+        partnerUserId: [{ required: true, message: '请输入paetnerID', trigger: 'blur' }],
+        org: [{ required: true, message: '请选择' }]
       },
       result: {
         example: true
       },
       socialsecurityList: [],
-      orgArea: undefined
+      orgArea: undefined,
+      openId: ''
     };
   },
   methods: {
+    getOrgInfo: function () {
+      var vm = this;
+      var p = new Promise(function (resolve, reject) {
+        vm.$http.get('api/rip/agencyInfo', {
+        params: {
+          organization_id: vm.inputFrom.org
+        },
+        headers: {
+          authorization: vm.$db.get('authorization')
+        }
+        }).then(function (res) {
+          resolve(res.data);
+        });
+      });
+      return p;
+    },
+    inputOrgInfo: function () {
+      var vm = this;
+      vm.getOrgInfo().then(function (data) {
+        const h = vm.$createElement;
+        vm.$msgbox({
+          title: '社保信息采集',
+          message: h(SsLogin, { props: { json: data } }),
+          showCancelButton: false,
+          confirmButtonText: '提交',
+          showClose: false,
+          beforeClose: (action, instance, done) => {
+            if (action === 'confirm') {
+              instance.confirmButtonLoading = true;
+              instance.confirmButtonText = '执行中...';
+              setTimeout(() => {
+                done();
+                setTimeout(() => {
+                  instance.confirmButtonLoading = false;
+                }, 300);
+              }, 3000);
+            } else {
+              done();
+            }
+          }
+        }).then(action => {
+          vm.$message({
+            type: 'info',
+            message: 'action: ' + action
+          });
+        });
+      });
+    },
+    getOpenId: function () {
+      var vm = this;
+      var p = new Promise(function (resolve, reject) {
+        vm.$http.get('api/rip/operatorReport', {
+        params: {
+          partner_user_id: vm.inputFrom.partnerUserId
+        },
+        headers: {
+          authorization: vm.$db.get('authorization')
+        }
+        }).then(function (res) {
+          console.log('获取openid：' + res.data.open_id);
+          resolve(res.data);
+        });
+      });
+      return p;
+    }, 
     onSubmit: function () {
       var vm = this;
-      console.log(vm.$router);
       vm.$refs['inputFrom'].validate((valid) => {
         if (valid) {
+          vm.getOpenId().then(function (data) {
+            vm.openId = data.open_id;
+          });
         }
       });
     }
---
+++ src/components/personal/python/SsLogin.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{json.data.name}}
+    <el-form :model="form" status-icon size="small" lable-width="0px" ref="form">
+      <el-form-item v-for="(e, index) in entrys" :key="index" :prop="e.name" >
+        <el-input :placeholder="e.hint" v-model="form[e.name]"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SsLogin',
+  components: {
+  },
+  props: {
+    json: {
+      type: Object
+    }
+  },
+  data () {
+    var entrys = this.json.data.tabs[0].entrys[0].parameters;
+    return {
+      entrys,
+      form: {}
+    };
+  }
+};
+</script>
