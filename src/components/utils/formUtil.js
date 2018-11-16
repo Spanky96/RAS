d4307af575bce526ef8ce4f319f132dc117fda94
Spanky Yang <819062398@qq.com>
2018-11-16 16:54:24 +0800

    运营商信用3结果还带继续

--- src/components/utils/formUtil.js
+++ src/components/utils/formUtil.js
@@ -1,3 +1,35 @@
+// 默认需要脱敏的字段
+const needTm = ['name', 'identityNo', 'mobile', 'callNum'];
+// 脱敏
+function handShortStr (val) {
+  return (new Array(val.length)).join("*") + val.slice(-1);
+}
+function handleMiddleStr (val) {
+  return val.slice(0, 1) + (new Array(val.length - 3)).join("*") + val.slice(-3);
+}
+function handleLongStr (val) {
+  return val.slice(0, 3) + (new Array(val.length - 6)).join("*") + val.slice(-4);
+}
+var getTmValue = function (k, v, tmArr) {
+  if (!v) {
+    return '';
+  }
+  !tmArr && (tmArr = needTm);
+  if (!k || (tmArr.includes(k) && !v.includes('*'))) {
+    // 需要脱敏
+    // 朱晓辉 -> **辉
+    // 18795905631 -> 187****5631
+    // 622281199606286774 -> 320***********6774
+    if (v.length <= 4) {
+      return handShortStr(v);
+    } else if (v.length <= 10) {
+      return handleMiddleStr(v);
+    } else {
+      return handleLongStr(v);
+    }
+  }
+  return v;
+};
 export default {
   // 下面是 结果转表单
   objectInfo2Html: function (basicInfo, kvs) {
@@ -41,6 +73,7 @@ export default {
     return html;
   },
   arrayInfo2HtmlV2: function (arrayInfo, kvs) {
+    // 表格形状，适合head比较少，条数比较多的情况
     var html = "";
     if (!arrayInfo || !(arrayInfo.constructor == Array)) {
       return "";
@@ -58,10 +91,35 @@ export default {
     }
     return html;
   },
+  arrayInfo2HtmlV3: function (arrayInfo, kvs, tmArr, format) {
+    // 表格形状，适合head比较少，条数比较多的情况（带脱敏功能）, 数据fomat功能
+    if (!tmArr) {
+      tmArr = needTm;
+    }
+    var html = "";
+    if (!arrayInfo || !(arrayInfo.constructor == Array)) {
+      return "";
+    }
+    html += this.kvsToTableHead(kvs);
+    arrayInfo.forEach(element => {
+      html += '<tr>';
+      for (let k in kvs) {
+        let value = getTmValue(k, element[k], tmArr);
+        if (format && format[k]) {
+          value = format[k](value);
+        }
+        html += `<td>${value}</td>`;
+      }
+      html += '</tr>';
+    });
+    if (arrayInfo.length == 0) {
+      html += `</tr><td colspan=${Object.keys(kvs).length} class="centerAlign">无数据</td></tr>`;
+    }
+    return html;
+  },
   getHtmlByKvsFromObj: function (kvs, obj) {
     var html = '';
     var keys = Object.keys(kvs);
-    // 0 1 2 3
     for (let i = 0; i < keys.length; i += 2) {
       let k1 = kvs[keys[i]];
       let v1 = obj[keys[i]] == undefined ? '' : obj[keys[i]];
@@ -80,5 +138,47 @@ export default {
               </tr>`;
     }
     return html;
+  },
+  getHtmlByKvsFromObjTm: function (kvs, obj, tmArr, format) {
+    // 能脱敏 还能格式化
+    if (!tmArr) {
+      tmArr = needTm;
+    }
+    var html = '';
+    var keys = Object.keys(kvs);
+    for (let i = 0; i < keys.length; i += 2) {
+      let k1 = kvs[keys[i]];
+      let v1 = getTmValue(keys[i], obj[keys[i]], tmArr);
+      format && format[keys[i]] && (v1 = format[keys[i]](v1));
+      let k2, v2;
+      if (i + 1 == keys.length) {
+        k2 = v2 = '';
+      } else {
+        k2 = kvs[keys[i + 1]];
+        v2 = getTmValue(keys[i + 1], obj[keys[i + 1]], tmArr);
+        format && format[keys[i + 1]] && (v2 = format[keys[i + 1]](v2));
+      }
+      html += `<tr>
+                <td width="20%">${k1}</td>
+                <td width="30%">${v1}</td>
+                <td width="20%">${k2}</td>
+                <td width="30%">${v2}</td>
+              </tr>`;
+    }
+    return html;
+  },
+  getTmValue,
+  arrayJoin: function (arr, needTm) {
+    if (!arr || arr.length == 0) {
+      return '无';
+    } else if (needTm) {
+      var newArr = [];
+      arr.map((n) => {
+        newArr.push(getTmValue(null, n));
+      });
+      return newArr.join(', ');
+    } else {
+      return arr.join(",");
+    }
   }
 };
